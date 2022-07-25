93fb2f5c5b543332a528b9c2d6227e1ee740db98
Badadak <toresalva2017@gmail.com>
2022-07-25 10:03:13 +0700

    Mengedit folder 03-Menulis Binary File, menambahkan folder 05 dan merename folder 05 -> 04

--- 03-Menulis Binary File/src/Main.cpp
+++ 03-Menulis Binary File/src/Main.cpp
@@ -1,10 +1,21 @@
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
 int main () {
 
-    cout << "Menulis Binary File\n" << endl;
-    
+    cout << "\nMenulis File Binary\n" << endl;
+
+    fstream myFile;
+
+    int angka = 1567;
+
+    myFile.open ("file.bin", ios::out | ios::binary);
+    myFile.write (reinterpret_cast<char*>(&angka), sizeof (angka));
+    myFile.close ();
+
     return 0;
 }
