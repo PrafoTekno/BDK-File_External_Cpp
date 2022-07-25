b96b5d1cb6432063998ff83e74f8f588a053dbac
Badadak <toresalva2017@gmail.com>
2022-07-25 10:20:46 +0700

    melakukan modifikasi pada folder 04 dan melakukan tracking

--- 04-Membaca Binary File/src/Main.cpp
+++ 04-Membaca Binary File/src/Main.cpp
@@ -1,12 +1,36 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 int main () {
 
-    cout << "Halo sobat" << endl;
-    cout << "Tes tes" << endl;
-    cout << "Baris 3\n" << endl;
+    cout << "\nMembaca File Binary\n" << endl;
+
+    fstream myFile;
+    int hasil;
+
+    myFile.open ("data.bin", ios::in | ios::binary);
+    myFile.read (reinterpret_cast<char*>(&hasil), sizeof (hasil));
+    myFile.close ();
+
+    cout << "Besar int = " << sizeof (int) << " byte" << endl;
+    cout << "Nilai dari file 'data.bin' = " << hasil << endl;
+
+    cout << endl;
 
     return 0;
+
 }
+    
+    /*
+    fstream myFile;
+
+    int tulis_bin = 5687;
+
+    myFile.open ("data.bin", ios::out | ios::binary);
+    myFile.write (reinterpret_cast<char*>(&tulis_bin), sizeof(tulis_bin));
+    myFile.close ();
+    */
